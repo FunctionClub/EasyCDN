8cd0a12e1795516547978c02421b28449b0157c4
unknown <wmy461737664@qq.com>
2016-10-22 20:37:48 +0800

    Support SSL

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# CDN
-CDN 服务器自动安装脚本
+#CDN 服务器自动安装脚本
+##说明
+采用Oneinstack包安装配置Nginx，此脚本方便做反向代理，CDN静态缓存资源（开发中）。
+##用法
+1.使用install.sh进行一键Nginx安装
+2.使用add.sh将本机变成CDN节点进行转发
+
+./add.sh
+
+What's the original server ip and port? (exp: http://1.1.1.1:80):
+<strong>这里输入你的源站IP和端口，例如： http://1.1.1.1:80</strong>
+
+What's the domain you want to use as the CDN doamin?(exp: ixh.me):
+<strong>这里输入本机反向代理的域名，例如：ixh.me</strong>
+
+Set up ssl to this proxy domain? ('y' or 'n'): 
+<strong>选择y或者n来选择本机是否启用SSL</strong>
+
+如果输入y则开始配置如下：
+
+Please input the location of the KEY
+Please input the location of the CRT
+<strong>这里分别输入你的绑定的反向代理域名SSL私钥和证书文件绝对路径</strong>
+Redirect all HTTP to HTTPS ? ('y' or 'n')
+<strong>选择y或者n来选择是否启用强制跳转所有http至https协议</strong>
--- add.sh
+++ add.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-read -p "What's the domain you want to proxy?[For Example: https://ixh.me:80 ]" res
-read -p "What's the domain you want to use?[For Example: ixh.me ]" dn
-
+clear
+#Read basic information for proxy
+read -p "What's the original server ip and port? (exp: http://1.1.1.1:80): " res
+read -p "What's the domain you want to use as the CDN doamin?(exp: ixh.me): " dn
+#choose whether to use SSL.
+while :; do echo
+    read -p "Set up ssl to this proxy domain? ('y' or 'n'): " ifssl
+    if [[ ! $ifssl =~ ^[y,n]$ ]];then
+        echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
+    else
+        if [ "$ifssl" == 'y' ];then
+        	echo "You choose to install SSL certificate."
+        	read -p "Please input the location of the KEY (exp: /root/ssl/ixh.me.key): " key
+        	read -p "Please input the location of the CRT (exp: /root/ssl/ixh.me.crt): " crt
+        	while :; do echo
+        		read -p "Redirect all HTTP to HTTPS ? ('y' or 'n'): " ifforcessl
+        		if [[ ! $ifforcessl =~ ^[y,n]$ ]];then
+        		echo "${CWARNING}input error! Please only input 'y' or 'n'${CEND}"
+    			else
+    				break
+    			fi
+        	done
+        	break
+        else
+        	echo "You choose NOT to install SSL certificate."
+        	break
+        fi
+    fi
+done
 #Configure Nginx
 cd /usr/local/nginx/conf/vhost/
 rm -rf $dn.conf
 touch $dn.conf
 echo "server {  " > $dn.conf
+echo "listen 80;" >> $dn.conf
+if [ "$ifssl" == 'y' ];then
+	echo "listen 443 ssl http2;" >> $dn.conf
+	echo "ssl_certificate $crt;" >> $dn.conf
+	echo "ssl_certificate_key $key;" >> $dn.conf
+	echo "ssl_protocols TLSv1 TLSv1.1 TLSv1.2;" >> $dn.conf
+	echo "ssl_ciphers EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5;" >> $dn.conf
+	echo "ssl_prefer_server_ciphers on;" >> $dn.conf
+	echo "ssl_session_timeout 10m;" >> $dn.conf
+	echo "ssl_session_cache builtin:1000 shared:SSL:10m;" >> $dn.conf
+	echo "ssl_buffer_size 1400;" >> $dn.conf
+	echo "add_header Strict-Transport-Security max-age=15768000;" >> $dn.conf
+	echo "ssl_stapling on;" >> $dn.conf
+	echo "ssl_stapling_verify on;" >> $dn.conf
+fi
+
+if [ "$ifforcessl" == 'y' ];then
+	echo "if ($ssl_protocol = "") { return 301 https://\$host\$request_uri; }" >> $dn.conf
+fi
 echo "server_name $dn;" >> $dn.conf
 echo "location / {" >> $dn.conf
 echo "proxy_set_header   X-Real-IP \$remote_addr;" >> $dn.conf
